f91b79b72b515a282e224cefc87ce87a6368a313
LiamVicary <149588879+LiamVicary@users.noreply.github.com>
2025-06-26 22:19:01 +0100

    Add corrections map for non-API safe names

--- project1/PHP/get_countries.php
+++ project1/PHP/get_countries.php
@@ -16,6 +16,34 @@ foreach ($geo['features'] as $feat) {
   }
 }
 
+// Correct any names for APIs
+$corrections = [
+  'Bosnia and Herz.'                 => 'Bosnia and Herzegovina',
+  'Central African Rep.'             => 'Central African Republic',
+  'Czech Rep.'             => 'Czech Republic',
+  'Côte d\'Ivoire'             => 'Ivory Coast',
+  'Dem. Rep. Congo'             => 'Democratic Republic of the Congo',
+  'Dem. Rep. Korea'             => 'North Korea',
+  'Dominican Rep.'      => 'Dominican Republic',
+  'Eq. Guinea'             => 'Equatorial Guinea',
+  'Falkland Is.'             => 'Falkland Islands',
+  'Korea'             => 'South Korea',
+  'Lao PDR'             => 'Laos',
+  'N. Cyprus'             => 'Cyprus',
+  'Solomon Is.'             => 'Solomon Islands',
+  'S. Sudan'             => 'South Sudan',
+  'W. Sahara'             => 'Western Sahara',
+  ''             => '',
+];
+
+$out = array_map(function($c) use ($corrections) {
+  if (isset($corrections[$c['name']])) {
+      $c['name'] = $corrections[$c['name']];
+  }
+  return $c;
+}, $out);
+
+
 // Sort by country name, ascending
 usort($out, function($a, $b) {
   return strcmp($a['name'], $b['name']);
