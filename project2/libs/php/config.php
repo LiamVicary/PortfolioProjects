b61b14e36a72512155b642a28450d9533f2755c5
LiamVicary <149588879+LiamVicary@users.noreply.github.com>
2025-08-19 15:26:20 +0100

    Create config.php

---
+++ project2/libs/php/config.php
@@ -0,0 +1,27 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "companydirectory";
+	$cd_user = "root";
+	$cd_password = "";
+
+
+	$conn = new mysqli(
+    $cd_host,
+    $cd_user,
+    $cd_password,
+    $cd_dbname,
+    $cd_port,
+    $cd_socket
+);
+
+
+
